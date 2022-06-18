de58f9ee12420d12cc23d35fbc92d12fd0662f90
dubandro <dubovik@internet.ru>
2022-06-18 13:51:27 +0200

    arch optimization

--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -1,21 +1,10 @@
 import java.io.*;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.file.Files;
 
-public class Server {
-    private final String DIR = "/Users/andrejdubovik/Desktop/Server/";
-//    private final String DIR = "../FileTransferIO/ServerIO/src/main/resources/";
-    private Socket socket = null;
-    private ObjectInputStream inputObject;
-    private ObjectOutputStream outputObject;
-    private OutputStream outputFile;
-    private File rFile;
-    private File sFile;
-    private int receiveNum;
-    private long totalWrite;
+public class Server extends FileTransfer {
 
     public Server (int port) {
+        DIR = "/Users/andrejdubovik/Desktop/Server/";
         System.out.println("Server started...");
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             socket = serverSocket.accept();
@@ -59,96 +48,16 @@ public class Server {
         }
     }
 
-    private void request(MessageType messageType, String fileName) {
-        ServiceMessage msg = new ServiceMessage(messageType, fileName);
-        try {
-            outputObject.writeObject(msg);
-            outputObject.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    private void receiveReady(String fileName) throws IOException {
-        rFile = new File(DIR, fileName);
-        if (rFile.exists()) System.out.println("File will be rewriting");
-        outputFile = Files.newOutputStream(rFile.toPath());
-        receiveNum = 0;
-        totalWrite = 0;
-
+    @Override
+    public void receiveReady(String fileName) throws IOException {
+        super.receiveReady(fileName);
         request(MessageType.SENDING_FILE, fileName);
     }
-    private void sendingReady(String fileName) {
-        sFile = new File(DIR, fileName);
-        if (!sFile.exists()) System.out.println("File not found!");
 
+    @Override
+    public void sendingReady(String fileName) {
+        super.sendingReady(fileName);
         if (sFile.exists()) new Thread(this::sendingFile).start();
         else request(MessageType.SENDING_ERROR, fileName);
-    }
-
-    public void receiveFile(ServiceMessage msg) {
-        if (receiveNum == msg.getCurrentPart()) {
-            try {
-                outputFile.write(msg.getBody());
-                outputFile.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            receiveNum++;
-            totalWrite += msg.getBody().length;
-        }
-        else {
-             //если не равны номера части которая должна быть и которая пришла - дать команду вернуться к предыдущей
-        }
-        if (totalWrite == msg.getFileSize()) {
-            System.out.println("File transfer from client completed, transferred " + totalWrite + " bytes");
-        }
-    }
-
-    public void sendingFile() {
-        try (DataInputStream readData = new DataInputStream(Files.newInputStream(sFile.toPath()))) {
-            long splitSize = splitSize();
-            long fileSize = sFile.length();
-            int totalParts = (int) Math.ceil(fileSize / splitSize);
-            long remainBytes = fileSize % splitSize;
-            long currentLength;
-            int currentRead;
-            long totalRead = 0;
-            int currentPart = 0;
-            do {
-                currentLength = currentPart < totalParts ? splitSize : remainBytes;
-                byte[] arrayData = new byte[(int) currentLength];
-                currentRead = readData.read(arrayData, 0, (int) currentLength);
-                if (currentRead != -1) {
-                    ServiceMessage msg = new ServiceMessage(MessageType.RECEIVE_FILE, fileSize, currentPart, arrayData);
-                    outputObject.writeObject(msg);
-                    outputObject.flush();
-                    totalRead += currentRead;
-                    currentPart++;
-                }
-            } while (currentPart <= totalParts);
-            if (totalRead == fileSize) {
-                System.out.println("File transfer to client completed, transferred " + totalRead + " bytes");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private int splitSize() {
-        return 8 * 1024;
-    }
-    private void closeConnection() {
-        try {
-            inputObject.close();
-            outputObject.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            socket.close();
-            if (socket.isClosed()) System.out.println("Socket close, client disconnected");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
